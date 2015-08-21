c3fd8dda34a04bee276936a61b7c5a1add7bc763
ngzHappy <zhaixueqiang@hotmail.com>
2015-08-21 08:53:20 +0800

    version 000.009 add TextureBuffer

--- src/OpenGLTool/OpenGLTool/OpenGLAPIPrivate/TexttureOpenglTool.hpp
+++ src/OpenGLTool/OpenGLTool/OpenGLAPIPrivate/TexttureOpenglTool.hpp
@@ -2,6 +2,7 @@
 #define __TEXTURE__opengltool__type__0x00
 
 #include "BaseType.hpp"
+#include "BufferType.hpp"
 
 namespace gl{
 
@@ -93,6 +94,43 @@ gsampler2DMSArray​ 	GL_TEXTURE_2D_MULTISAMPLE_ARRAY 	Multisample Array Texture
 		explicit operator const UnsignedInteger&()const{ return value_; }
 		explicit Texture(const UnsignedInteger v) :value_(v){}
 		explicit operator bool()const{ return value_ != 0; }
+
+        enum class InternalFormat : GLenum{
+            R8      =GL_R8       ,
+            R16     =GL_R16      ,
+            R16F    =GL_R16F     ,
+            R32F    =GL_R32F     ,
+            R8I     =GL_R8I      ,
+            R16I    =GL_R16I     ,
+            R32I    =GL_R32I     ,
+            R8UI    =GL_R8UI     ,
+            R16UI   =GL_R16UI    ,
+            R32UI   =GL_R32UI    ,
+            RG8     =GL_RG8      ,
+            RG16    =GL_RG16     ,
+            RG16F   =GL_RG16F    ,
+            RG32F   =GL_RG32F    ,
+            RG8I    =GL_RG8I     ,
+            RG16I   =GL_RG16I    ,
+            RG32I   =GL_RG32I    ,
+            RG8UI   =GL_RG8UI    ,
+            RG16UI  =GL_RG16UI   ,
+            RG32UI  =GL_RG32UI   ,
+            RGB32F  =GL_RGB32F   ,
+            RGB32I  =GL_RGB32I   ,
+            RGB32UI =GL_RGB32UI  ,
+            RGBA8   =GL_RGBA8    ,
+            RGBA16  =GL_RGBA16   ,
+            RGBA16F =GL_RGBA16F  ,
+            RGBA32F =GL_RGBA32F  ,
+            RGBA8I  =GL_RGBA8I   ,
+            RGBA16I =GL_RGBA16I  ,
+            RGBA32I =GL_RGBA32I  ,
+            RGBA8UI =GL_RGBA8UI  ,
+            RGBA16UI=GL_RGBA16UI ,
+            RGBA32UI=GL_RGBA32UI ,
+        };
+
 	};
 
     class NamedTexture :public Texture {
@@ -141,6 +179,17 @@ gsampler2DMSArray​ 	GL_TEXTURE_2D_MULTISAMPLE_ARRAY 	Multisample Array Texture
 				glBindTextureUnit(i, texture_);
 			}
 
+
+            void bind(
+                    InternalFormat  internalformat,
+                    gl::NamedBuffer buffer
+                      ){
+                glTextureBuffer(
+                            texture_,
+                            GLenum(internalformat),
+                            GLuint(buffer)
+                            );
+            }
     };
 
 
